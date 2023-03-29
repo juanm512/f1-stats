a3d0ff0d63bab0252937a033c9d00e0c5243aead
juanm512 <512juanm@gmail.com>
2023-03-29 00:21:35 -0300

    Changed all images imports to links in cloudnary

--- utils/getTeamsData.js
+++ utils/getTeamsData.js
@@ -1,24 +1,24 @@
 export const CarsImages = {
-  // alfa: require("../assets/cars/alfa_car.png"),
-  // alphatauri: require("../assets/cars/alphatauri_car.avif"),
-  // alpine: require("../assets/cars/alpine_car.avif"),
-  // aston_martin: require("../assets/cars/aston_martin_car.png"),
-  // ferrari: require("../assets/cars/ferrari_car.avif"),
-  // haas: require("../assets/cars/haas_car.avif"),
-  // mclaren: require("../assets/cars/mclaren_car.avif"),
-  // mercedes: require("../assets/cars/mercedes_car.avif"),
-  // red_bull: require("../assets/cars/red_bull_car.png"),
-  // williams: require("../assets/cars/williams_car.avif")
-  mercedes: "https://i.ibb.co/t25KpZf/mercedes-car.png",
-  red_bull: "https://i.ibb.co/fQxSD46/red-bull-car.png",
-  williams: "https://i.ibb.co/hsnCfm7/williams-car.png",
-  alphatauri: "https://i.ibb.co/xmCw5QD/alphatauri-car.png",
-  alpine: "https://i.ibb.co/1dZ11KX/alpine-car.png",
-  ferrari: "https://i.ibb.co/tszh8Hx/ferrari-car.png",
-  haas: "https://i.ibb.co/qjh0S6P/haas-car.png",
-  mclaren: "https://i.ibb.co/LnpqZP7/mclaren-car.png",
-  aston_martin: "https://i.ibb.co/KNGYqNn/aston-martin-car.png",
-  alfa: "https://i.ibb.co/8sPZfsP/alfa-car.png"
+  alfa: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059406/f1-stats/cars/alfa_car_isnphf.png",
+  alphatauri: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059406/f1-stats/cars/alphatauri_car_noim0w.png",
+  alpine: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059405/f1-stats/cars/alpine_car_pstaw2.png",
+  aston_martin: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059405/f1-stats/cars/aston_martin_car_eunrt7.png",
+  ferrari: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059405/f1-stats/cars/ferrari_car_c66yvg.png",
+  haas: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059405/f1-stats/cars/haas_car_ireuwx.png",
+  mclaren: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059405/f1-stats/cars/mclaren_car_yqmo7d.png",
+  mercedes: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059405/f1-stats/cars/mercedes_car_bhkcje.png",
+  red_bull: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059406/f1-stats/cars/red_bull_car_nbg4st.png",
+  williams: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059406/f1-stats/cars/williams_car_qbfzod.png"
+  // mercedes: "https://i.ibb.co/t25KpZf/mercedes-car.png",
+  // red_bull: "https://i.ibb.co/fQxSD46/red-bull-car.png",
+  // williams: "https://i.ibb.co/hsnCfm7/williams-car.png",
+  // alphatauri: "https://i.ibb.co/xmCw5QD/alphatauri-car.png",
+  // alpine: "https://i.ibb.co/1dZ11KX/alpine-car.png",
+  // ferrari: "https://i.ibb.co/tszh8Hx/ferrari-car.png",
+  // haas: "https://i.ibb.co/qjh0S6P/haas-car.png",
+  // mclaren: "https://i.ibb.co/LnpqZP7/mclaren-car.png",
+  // aston_martin: "https://i.ibb.co/KNGYqNn/aston-martin-car.png",
+  // alfa: "https://i.ibb.co/8sPZfsP/alfa-car.png"
 }
 
 // export const TeamsTinyImages = {
@@ -35,16 +35,26 @@ export const CarsImages = {
 // }
 
 export const TeamsImages = {
-  alfa: require("../assets/teams/alfa.png"),
-  alphatauri: require("../assets/teams/alphatauri.svg"),
-  alpine: require("../assets/teams/alpine.png"),
-  aston_martin: require("../assets/teams/aston_martin.png"),
-  ferrari: require("../assets/teams/ferrari.png"),
-  haas: require("../assets/teams/haas.png"),
-  mclaren: require("../assets/teams/mclaren.png"),
-  mercedes: require("../assets/teams/mercedes.png"),
-  red_bull: require("../assets/teams/red_bull.svg"),
-  williams: require("../assets/teams/williams.webp")
+  // alfa: require("../assets/teams/alfa.png"),
+  // alphatauri: require("../assets/teams/alphatauri.svg"),
+  // alpine: require("../assets/teams/alpine.png"),
+  // aston_martin: require("../assets/teams/aston_martin.png"),
+  // ferrari: require("../assets/teams/ferrari.png"),
+  // haas: require("../assets/teams/haas.png"),
+  // mclaren: require("../assets/teams/mclaren.png"),
+  // mercedes: require("../assets/teams/mercedes.png"),
+  // red_bull: require("../assets/teams/red_bull.svg"),
+  // williams: require("../assets/teams/williams.webp")
+  alfa: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059580/f1-stats/teams/alfa_sdq0ax.png",
+  alphatauri: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059580/f1-stats/teams/alphatauri_ki0gyd.svg",
+  alpine: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059579/f1-stats/teams/alpine_oaoqvk.png",
+  aston_martin: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059579/f1-stats/teams/aston_martin_mgkaht.png",
+  ferrari: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059581/f1-stats/teams/ferrari_g2iun4.png",
+  haas: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059581/f1-stats/teams/haas_lubupg.png",
+  mclaren: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059580/f1-stats/teams/mclaren_hcmpg4.png",
+  mercedes: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059580/f1-stats/teams/mercedes_bvbktt.png",
+  red_bull: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059580/f1-stats/teams/red_bull_tynwt2.svg",
+  williams: "https://res.cloudinary.com/dvjbhwt2l/image/upload/v1680059580/f1-stats/teams/williams_sct14l.webp"
 }
 
 export const TeamsData = {
